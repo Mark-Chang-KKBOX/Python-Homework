5d3eedecfa8d29b2588fcc1861f3a4250f0141c4
Mark-Chang-KKBOX <markchang@kkbox.com>
2014-04-25 21:51:43 +0800

    no message

--- 20140425.py
+++ 20140425.py
@@ -1,9 +1,13 @@
+import sys
 
-unsort = [2134, 3412, 6421, 8723, 9239,1234, 2345]
+number = []
+for i in range(1, len(sys.argv)):
+	number.append(int(sys.argv[i]))
 
-temp = unsort[0]
-unsort[0] = unsort[1]
-unsort[1] = temp
-
-for aa in unsort:
-	print("%d, " % a)
+for i in range(0, len(number)):
+	for j in range(i, len(number)):
+		if number[i] < number[j]:
+			number[i], number[j] = number[j], number[i]
+	
+for i in range(0, 3):
+	print ("%d" % number[i])
