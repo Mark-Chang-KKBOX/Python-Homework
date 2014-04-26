e4f93e0b7d02bc4ff0e17065b7d5e94fd1537c85
Mark-Chang-KKBOX <markchang@kkbox.com>
2014-04-27 00:21:03 +0800

    no message

--- 20140425.py
+++ 20140425.py
@@ -1,16 +1,33 @@
+import os
+import re
+
+inputFileName = "inputNumber.txt"
+outputFileName = "outputNumber.txt"
 
 def xSort(unsorted):
 	sorted = [0] * (max(unsorted) + 1)
 	for i in range(0, len(unsorted)):
 		sorted[unsorted[i]] = unsorted[i]
 	return sorted
-	
 
-number = [2134, 3412, 6421, 8723, 9239, 1234, 2345]
+with open(inputFileName) as file:
+	number = file.read().splitlines()
+for i in range(len(number)):
+	number[i] = int(number[i])
+
+	
 sorted = xSort(number)
+
+try:
+    os.remove(outputFileName)
+except OSError:
+    pass
 
 count = 0
 for i in range(max(sorted), -1, -1):
 	if sorted[i] > 0 and count < 3:
 		count = count + 1
 		print(sorted[i])
+		f = open(outputFileName, "a")
+		f.write(str(sorted[i])+'\n')
+		f.close()
