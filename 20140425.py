2f1e7160f263828352e1a68dcc406d68339ad7ca
Mark-Chang-KKBOX <markchang@kkbox.com>
2014-04-27 10:08:28 +0800

    no message

--- 20140425.py
+++ 20140425.py
@@ -7,20 +7,18 @@ def readNumberFromFile(fileName):
 		number[i] = int(number[i])
 	return number
 
-def writeThreeBiggestNumberToFile(fileName, sortedNumber):
+
+def writeNumberToFile(fileName, number):
 	try:
 		os.remove(fileName)
 	except OSError:
 		pass
 
-	count = 0
-	for i in range(max(sortedNumber), -1, -1):
-		if sortedNumber[i] > 0 and count < 3:
-			count = count + 1
-			print(sortedNumber[i])
-			f = open(fileName, "a")
-			f.write(str(sortedNumber[i])+'\n')
-			f.close()
+	for i in range(len(number)):
+		print(number[i])
+		f = open(fileName, "a")
+		f.write(str(number[i])+'\n')
+		f.close()
 	
 def xSort(unsorted):
 	sorted = [0] * (max(unsorted) + 1)
@@ -28,7 +26,21 @@ def xSort(unsorted):
 		sorted[unsorted[i]] = unsorted[i]
 	return sorted
 
+
+threeBiggestNumber = []
+def getMaxNumber(number):
+	maxValue = number[0]
+	for i in range(1, len(number)):
+		if number[i] > maxValue:
+			maxValue = number[i]
+	
+	threeBiggestNumber.append(maxValue)
+	number.remove(maxValue)
+	
 if __name__ == '__main__':
 	number = readNumberFromFile(sys.argv[1])
-	sortedNumber = xSort(number)
-	writeThreeBiggestNumberToFile("outputNumber.dat", sortedNumber)
+	getMaxNumber(number)
+	getMaxNumber(number)
+	getMaxNumber(number)
+	writeNumberToFile("outputNumber.dat", threeBiggestNumber)
+	
