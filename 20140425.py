413880b4fdc7406868c50c4cf80ac8b68efad94d
Mark-Chang-KKBOX <markchang@kkbox.com>
2014-04-27 09:14:40 +0800

    no message

--- 20140425.py
+++ 20140425.py
@@ -1,33 +1,34 @@
-import os
-import re
-import sys
+import os, sys
 
-inputFileName = sys.argv[1]
-outputFileName = "outputNumber.txt"
+def readNumberFromFile(fileName):
+	with open(fileName) as file:
+		number = file.read().splitlines()
+	for i in range(len(number)):
+		number[i] = int(number[i])
+	return number
 
+def writeThreeBiggestNumberToFile(fileName, sorted):
+	try:
+		os.remove(fileName)
+	except OSError:
+		pass
+
+	count = 0
+	for i in range(max(sorted), -1, -1):
+		if sorted[i] > 0 and count < 3:
+			count = count + 1
+			print(sorted[i])
+			f = open(fileName, "a")
+			f.write(str(sorted[i])+'\n')
+			f.close()
+	
 def xSort(unsorted):
 	sorted = [0] * (max(unsorted) + 1)
 	for i in range(0, len(unsorted)):
 		sorted[unsorted[i]] = unsorted[i]
 	return sorted
 
-with open(inputFileName) as file:
-	number = file.read().splitlines()
-for i in range(len(number)):
-	number[i] = int(number[i])
-	
-sorted = xSort(number)
-
-try:
-    os.remove(outputFileName)
-except OSError:
-    pass
-
-count = 0
-for i in range(max(sorted), -1, -1):
-	if sorted[i] > 0 and count < 3:
-		count = count + 1
-		print(sorted[i])
-		f = open(outputFileName, "a")
-		f.write(str(sorted[i])+'\n')
-		f.close()
+if __name__ == '__main__':
+	number = readNumberFromFile(sys.argv[1])
+	sorted = xSort(number)
+	writeThreeBiggestNumberToFile("outputNumber.txt", sorted)
