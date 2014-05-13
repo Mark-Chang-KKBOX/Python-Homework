d3759fcd738a931b2cd871123680346c13b1fff7
Mark-Chang-KKBOX <markchang@kkbox.com>
2014-05-13 10:46:01 +0800

    no message

---
+++ test.py
@@ -0,0 +1,20 @@
+import sys
+
+def isValid(inString):
+	if len(inString) == 0:
+		return False
+	
+	count = 0
+	for x in [y for y in [z for z in inString.split('a>')] if y]:
+		if x == "<":
+			count = count + 1;
+		elif x == "</":				
+			count = count - 1;
+			if count < 0:
+				return False
+		else:
+			return False
+	return count == 0
+
+if __name__ == '__main__':
+	print(isValid(sys.argv[1]))
