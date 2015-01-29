e5ae33c490432920e6f044c4b27225cc6731c351
MarkChang <markchang@KKBOXdeMacBook-Pro-5.local>
2015-01-29 14:09:24 +0800

    no message

--- 20150114/20150114.py
+++ 20150114/20150114.py
@@ -1,5 +1,24 @@
+﻿# -*- coding: utf-8 -*-
+
+def translate(str):
+	t = str
+	t = t.replace("*", "乘")
+	t = t.replace("=", "等於")	
+	t = t.replace("0", "零")
+	t = t.replace("1", "一")
+	t = t.replace("2", "二")
+	t = t.replace("3", "三")
+	t = t.replace("4", "四")
+	t = t.replace("5", "五")
+	t = t.replace("6", "六")
+	t = t.replace("7", "七")
+	t = t.replace("8", "八")
+	t = t.replace("9", "九")
+	return t
+
 for x in range(1, 10):
 	s = ""
 	for y in range(1, 10):
-		s += "%s*%s=%s\t" % (x, y, x * y)
-	print(s)
+		s = "%s*%s=%s" % (x, y, x * y)
+		print(translate(s))
+	print("")
