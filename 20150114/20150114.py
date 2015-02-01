6f14270e64eadd06f7c070feddef541a2748f753
MarkChang <markchang@KKBOXdeMacBook-Pro.local>
2015-02-01 19:42:21 +0800

    no message

--- 20150114/20150114.py
+++ 20150114/20150114.py
@@ -1,24 +1,54 @@
 ﻿# -*- coding: utf-8 -*-
 
-def translate(str):
-	t = str
-	t = t.replace("*", "乘")
-	t = t.replace("=", "等於")	
-	t = t.replace("0", "零")
-	t = t.replace("1", "一")
-	t = t.replace("2", "二")
-	t = t.replace("3", "三")
-	t = t.replace("4", "四")
-	t = t.replace("5", "五")
-	t = t.replace("6", "六")
-	t = t.replace("7", "七")
-	t = t.replace("8", "八")
-	t = t.replace("9", "九")
-	return t
+def translate_string(string):
+	ts = string
+
+	ts = ts.replace("*", "乘")
+	ts = ts.replace("=", "等於")
+
+	ts = ts.replace("0", "零")
+	ts = ts.replace("1", "一")
+	ts = ts.replace("2", "二")
+	ts = ts.replace("3", "三")
+	ts = ts.replace("4", "四")
+	ts = ts.replace("5", "五")
+	ts = ts.replace("6", "六")
+	ts = ts.replace("7", "七")
+	ts = ts.replace("8", "八")
+	ts = ts.replace("9", "九")
+
+	return ts
+
+def translate_number(number):
+
+	if number == 10:
+		return "十"
+
+	if number > 10 and number < 20:
+		return translate_string("十" + str(number % 10))
+
+	unit = ["", "十"]
+
+	s = str(number)
+	a = ""
+
+	while len(s) > 0:
+		a += s[0] + unit[len(s) - 1]
+		s = s[1:]
+	
+	ts = a
+
+	if ts.endswith("0"):
+		ts = ts[0:len(ts) - 1]
+		
+	return ts
 
 for x in range(1, 10):
 	s = ""
 	for y in range(1, 10):
-		s = "%s*%s=%s" % (x, y, x * y)
-		print(translate(s))
-	print("")
+		s = "%s*%s=%s" % (translate_number(x), 
+			              translate_number(y), 
+			              translate_number(x * y))
+		print(translate_string(s))
+	print
+
